929d470d78f159c224e6b90fd2c11d12d45f42d3
capomanpc <121809222+capomanpc@users.noreply.github.com>
2023-06-04 15:31:26 +0900

    ab

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 //無名関数-NoNameFunction-
 
 func main() {
-	func
+	f := func(x, y int) int {
+		return x + y
+	}
+	i := f(1, 2)
+	fmt.Println(i)
+
+	func(x, y int) int {
+		return x + y
+	}(1, 2)
+	//関数の最後に(1,2)を追加することで引数を指定できる
+
 }
